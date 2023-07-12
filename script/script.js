353ab10fc64ebd386a711e447aa409ab7d62c130
MicheleGresta <malachia_1997@hotmail.it>
2023-07-12 19:18:17 +0200

    stop

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 `use strict`
-
+let play = true;
 const btnPlay = document.querySelector(".btn-play");
 const contenitoreGioco = document.querySelector(".container-small");
 
@@ -10,6 +10,7 @@ btnPlay.addEventListener("click", bottoneGioca);
 
 function bottoneGioca() {
     contenitoreGioco.innerHTML = "";
+    play = true;
     const difficulty = document.getElementById("select-level").value;
     console.log(difficulty);
 
@@ -21,7 +22,7 @@ function bottoneGioca() {
         numeroCaselle = 49;
     }
     creaCaselle();
-    creaBombe();
+
 
 };
 
@@ -30,7 +31,10 @@ function creaBombe() {
     const totBombe = [];
     for (let i = 0; i < 16; i++) {
         const bombaRandom = (Math.floor(Math.random() * numeroCaselle) + 1);
-         totBombe.push(bombaRandom);
+
+        if ((totBombe.indexOf(bombaRandom)) === -1) {
+            totBombe.push(bombaRandom);
+        } else { i-- };
     }
     console.log(totBombe);
 
@@ -41,23 +45,40 @@ function creaBombe() {
 
 
 function creaCaselle() {
+    const arrayBombe = creaBombe();
+
     for (let i = 0; i < numeroCaselle; i++) {
 
 
         const square = document.createElement("div");
-        const squaresPerRow = Math.sqrt(numeroCaselle);       
+        const squaresPerRow = Math.sqrt(numeroCaselle);
 
         square.classList.add("squares");
         // square.textContent = (i + 1).toString();
         square.style.flexBasis = `calc(100% / ${squaresPerRow})`;
-        
+
         // numero le caselle "non visibilmente"
-        square.dataset.numCasella = ((i+1).toString());
-        
+        square.dataset.numCasella = ((i + 1).toString());
+
         ////////////////////
 
         square.addEventListener("click", function () {
+
+            if (play === false) {
+                return
+            }
             square.classList.toggle("toggleColor");
+
+            for (let j = 0; j < 16; j++) {
+
+                if (arrayBombe[j] === i + 1) {
+                    square.classList.toggle("toggleBomba");
+                    square.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+                    console.log(arrayBombe[j]);
+                    play = false;
+                }
+
+            }
         })
         contenitoreGioco.append(square);
     }
